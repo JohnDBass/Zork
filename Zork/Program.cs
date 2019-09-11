f4a973f22cfcb2c79a4011d3bdc96562ce2b535d
JohnDBass <48814518+JohnDBass@users.noreply.github.com>
2019-09-11 15:20:27 -0400

    in progress 5

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 namespace Zork
 {
     class Program
-    {        
+    {
+        private static string[] Rooms =
+            {"Forest", "West of House", "Behind House", "Clearing", "Canyon View"};
+
+        private int currentRoom;
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
             Commands command = Commands.UNKNOWN;
+            currentRoom = 1;
+
             while (command != Commands.QUIT)
             {
                 Console.Write("> ");
@@ -23,7 +31,7 @@ namespace Zork
                 {
                     case Commands.LOOK:
                         outputString = "This is an open field west of a white house, with a boarded front door." +
-                            " \nA rubber mat saying 'Welcome to Zork!' lies by the door";                                                
+                            " \n A rubber mat saying 'Welcome to Zork!' lies by the door";                                                
                         break;
 
                     case Commands.QUIT:
@@ -32,9 +40,12 @@ namespace Zork
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
+                        outputString = "The way is shut.";
+                        break;
+
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = $"You moved {command}.";
+                        outputString = $"You moved {command}. \n {Rooms[currentRoom]}";
                         break;
 
                     default:
@@ -49,5 +60,7 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => 
             (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+
+        private 
     }
 }
