ca98f511f580c4eef8545247e14868c0a3e975df
JohnDBass <48814518+JohnDBass@users.noreply.github.com>
2019-09-11 13:57:31 -0400

    Update Program.cs
    Module 2

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,9 +8,36 @@ namespace Zork
 {
     class Program
     {
+        enum Commands
+        {
+            QUIT,
+            LOOK,
+            NORTH,
+            SOUTH,
+            EAST,
+            WEST,
+            UNKNOWN
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string inputString = Console.ReadLine();
+            inputString = inputString.ToUpper();
+
+            if (inputString == "QUIT")
+            {
+                Console.WriteLine("Thank you for playing.");
+            }
+            else if (inputString == "LOOK")
+            {
+                Console.WriteLine("This is an open field west of a white house, with a boarded front door. " +
+                    "\nA rubber mat saying 'Welcome to Zork!' lies by the door.");                
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized command.");
+            }
         }
     }
 }
