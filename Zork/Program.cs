ae23bb97ca14841fde6ab63bd13ca88ae08a18b7
JohnDBass <48814518+JohnDBass@users.noreply.github.com>
2019-09-16 12:19:02 -0400

    Update Program.cs

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -11,56 +11,92 @@ namespace Zork
         private static string[] Rooms =
             {"Forest", "West of House", "Behind House", "Clearing", "Canyon View"};
 
-        private int currentRoom;
+        private static int roomColumn;
 
+        private static List<Commands> Directions = new List<Commands>
+        {
+            Commands.NORTH,
+            Commands.SOUTH,
+            Commands.EAST,
+            Commands.WEST
+        };
 
         static void Main(string[] args)
         {
+            roomColumn = 1;
             Console.WriteLine("Welcome to Zork!");
-
-            Commands command = Commands.UNKNOWN;
-            currentRoom = 1;
+            Commands command = Commands.UNKNOWN;            
 
             while (command != Commands.QUIT)
             {
+                Console.WriteLine(Rooms[roomColumn]);
                 Console.Write("> ");
                 command = ToCommand(Console.ReadLine().Trim());
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door." +
-                            " \n A rubber mat saying 'Welcome to Zork!' lies by the door";                                                
+                        Console.WriteLine("This is an open field west of a white house, with a boarded front door." +
+                            " \n A rubber mat saying 'Welcome to Zork!' lies by the door");                                                
                         break;
 
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
 
                     case Commands.NORTH:
-                    case Commands.SOUTH:
-                        outputString = "The way is shut.";
-                        break;
-
+                    case Commands.SOUTH:                    
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = $"You moved {command}. \n {Rooms[currentRoom]}";
+                        if (Move(command) == false)
+                        {
+                            Console.WriteLine("The Way is shut!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You moved " + (command) );
+                        }
                         break;
 
                     default:
-                        outputString = "Unknown command.";
+                        Console.WriteLine("Unknown command.");
                         break;
                 }
-
-                Console.WriteLine(outputString);
             }
 
         }
 
         private static Commands ToCommand(string commandString) => 
-            (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
+            (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);       
 
-        private 
+        private static bool Move(Commands command)
+        {
+            
+            bool canMove = true;
+            switch (command)
+            {
+                case Commands.NORTH:
+                    break;
+
+                case Commands.SOUTH:
+                    break;
+
+                case Commands.EAST when roomColumn < Rooms.GetLength(0) - 1:
+                    roomColumn++;
+                    break;
+
+                case Commands.WEST when roomColumn > 0:
+                    roomColumn--;
+                    break;
+
+                default:
+                    canMove = false;
+                    break;
+            }
+
+            return canMove;
+        }
+
+        
     }
 }
