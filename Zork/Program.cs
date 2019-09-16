27d3fae23e1e4dd7e201af2ee0fae884ff6d783b
JohnDBass <48814518+JohnDBass@users.noreply.github.com>
2019-09-16 14:16:44 -0400

    Module 8

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,15 +8,16 @@ namespace Zork
 {
     class Program
     {
-        private static string[,] Rooms = {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind house" },
-            { "Dense Woods", "North of House", "Clearing" },
+        private static readonly Room[,] Rooms =
+       {
+            { new Room("Rocky Trail"), new Room("South of House"),new Room("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
         private static (int Row, int Column) Location = (1, 1);
 
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -35,9 +36,12 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
             Commands command = Commands.UNKNOWN;
             while (command != Commands.QUIT)
+
+
             {
                 Console.WriteLine(CurrentRoom);
                 Console.Write("> ");
@@ -46,8 +50,7 @@ namespace Zork
                 switch (command)
                 {
                     case Commands.LOOK:
-                        Console.WriteLine("This is an open field west of a white house, with a boarded front door." +
-                            " \n A rubber mat saying 'Welcome to Zork!' lies by the door");                                                
+                        Console.WriteLine(CurrentRoom.Description);                                            
                         break;
 
                     case Commands.QUIT:
@@ -70,6 +73,20 @@ namespace Zork
                 }
             }
 
+        }
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";                                                                            //Rocky Trail
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";     //South of House
+            Rooms[0, 2].Description = "You are at the top of the Great Canyon on its south wall.";                                                  //Canyon View
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";                                                 //Forest
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";                                    //West of House
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar."; //Behind House
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";        //Dense Woods
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";     //North of House
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";                                //Clearing
         }
 
         private static Commands ToCommand(string commandString) => 
---
+++ Zork/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zork
+{
+    public class Room
+    {
+        public string Name { get; }
+
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public Room(string name,string description = "")
+        {
+            Name = name;
+            Description = description;
+        }
+
+       
+
+        
+    }
+}
