8804a163c2d7e08424409fedaed86e74c250cf52
JohnDBass <48814518+JohnDBass@users.noreply.github.com>
2019-09-30 11:48:39 -0400

    14 in progress

--- Zork/Game.cs
+++ Zork/Game.cs
@@ -1,19 +1,27 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.IO;
+using System.Linq;
+using System.Reflecton;
+using System.Text;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
 
 namespace Zork
 {
     
     public class Game
     {
+        [JsonIgnore]
+        public static Game Instance { get; private set; }
+
         public World World { get; set; }
 
         [JsonIgnore]
         public Player Player { get; private set; }
 
         [JsonIgnore]
-        private bool IsRunning { get; set; }
+        public bool IsRunning { get; }
 
         [JsonIgnore]
         public CommandManager CommandManager { get; }
@@ -24,33 +32,30 @@ namespace Zork
             Player = player;
         }
 
-        public Game()
+        public Game() => CommandManager = new CommandManager();
+        
+        public static void Start(string gameFilename)
         {
-            Command[] commands =
+            if (!gameFilename.Exists(gameFilename))
             {
-                new Command("LOOK", new string[] { "LOOK", "L" },
-                    (game, commandContext) => Console.WriteLine(game.Player.Location.Description)),
+                throw new FileNotFoundException("Expected file.", gameFilename);
+            }
 
-                new Command("QUIT", new string[] { "QUIT", "Q" },
-                    (game, commandContext) => game.IsRunning = false),
-
-                new Command("NORTH", new string[] { "NORTH", "N" }, MovementCommands.North),
-
-                new Command("SOUTH", new string[] { "SOUTH", "S" }, MovementCommands.South),
-
-                new Command("EAST", new string[] { "EAST", "E" }, MovementCommands.East),
-
-                new Command("WEST", new string[] { "WEST", "W" }, MovementCommands.West)
-            };
-
-            CommandManager = new CommandManager(commands);
+            while (Instance == null || Instance.mIsRestarting)
+            {
+                Instance = Load(gameFilename);
+                Instance.LoadCommands();
+                Instance.LoadScripts();
+                Instance.DisplayWelcomeMessage();
+                Instance.Run();
+            }
         }
 
-        public void Run()
+        private void Run()
         {
-            IsRunning = true;
+            mIsRunning = true;
             Room previousRoom = null;
-            while (IsRunning)
+            while (mIsRunning)
             {
                 Console.WriteLine(Player.Location);
                 if (previousRoom != Player.Location)
@@ -70,6 +75,16 @@ namespace Zork
                 }
             }
         }
+
+        public void Restart()
+        {
+            mIsRunning = false;
+            mIsRestarting = true;
+            Console.Clear();
+        }
+
+        public void Quit() => mIsRunning = false;
+
         public static Game Load(string filename)
         {
             Game game = JsonConvert.DeserializeObject<Game>(File.ReadAllText(filename));
@@ -77,5 +92,74 @@ namespace Zork
 
             return game;
         }        
+
+        private void LoadCommands()
+        {
+            var commandMethods = (from type in Assembly.GetExecutingAssembly().GetTypes()
+                                  from method in type.GetMethods()
+                                  let attribute = method.GetCustomAttribute<CommandAttribute>()
+                                  where type.IsClass && type.GetCustomAttribute<CommandClassAttribute>() != null
+                                  where attribute != null
+                                  select new Command(attribute.CommandName, attribute.Verbs,
+                                  (Action<Game, CommandContext>)Delegate.CreateDelegate(typeof(Action<Game, CommandContext>), method)));
+
+            CommandManager.AddCommands(commandMethods);
+        }
+
+        private void LoadScripts()
+        {
+            foreach (string file in Directory.EnumeratedFiles(ScriptDirectory, ScriptFileExtension))
+            {
+                try
+                {
+                    var scriptOptions = scriptOptions.Default.AddReferences(Assembly.GetExecutingAssembly());
+
+                    scriptOptions = scriptOptions.WithEmitDebugInformation(true)
+                                    .WithFilePath(new FileInfo(file).FullName)
+                                    .WithFileEncoding(Encoding.UTF8);
+
+                    string script = file.ReadAllText(file);
+                    CSharpScript.RunAsync(script, scriptOptions).Wait();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error compiling script: {file} Error: {ex.Message}");
+                }
+            }
+        }
+
+        public bool ConfirmAction(string prompt)
+        {
+            Console.Write(prompt);
+
+            while (true)
+            {
+                string response = Console.ReadLine().Trim().ToUpper();
+                if (response == "YES" || response == "Y")
+                {
+                    return true;
+                }
+                else if (response == "NO" || response == "N")
+                {
+                    return false;
+                }
+                else
+                {
+                    Console.Write("Please answer yes or no.> ");
+                }
+            }
+        }
+
+        private void DisplayWelcomeMessage() => Console.WriteLine(WelcomeMessage);
+
+        public static readonly Random Random = new Random();
+        private static readonly string ScriptDirectory = "Scripts";
+        private static readonly string ScriptFileExtension = "*.csx";
+
+        [JsonProperty]
+        private string WElcomeMessage = null;
+
+        private bool mIsRunning;
+        private bool mIsRestarting;
     }
 }
